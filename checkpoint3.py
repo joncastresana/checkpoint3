f6f2241b2a8d1e374914c4ca67bbfe74d1ea72c0
joncastresana <151607305+joncastresana@users.noreply.github.com>
2024-02-20 11:35:04 +0100

    Add files via upload

---
+++ checkpoint3.py
@@ -0,0 +1,69 @@
+
+# Exercise 1:
+
+cadena = "Hola mundo"
+numero = 1
+lista = [1,2,3]
+respuesta = True
+
+# Exercise 2:
+
+nueva_cadena = cadena[0:3]
+
+# Exercise 3: 
+
+lista[0]
+
+# Exercise 4:
+
+nuevo_numero = numero + 10
+
+# Exercise 5: 
+
+lista[-1]
+
+# Exercise 6:
+
+names = 'harry,alex,susie,jared,gail,conner'
+names = names.split(",")
+
+# Exercise 7:
+
+get_word = (cadena[0:4]).upper()
+new_string = get_word + cadena[4:]
+
+# Exercise 8:
+
+print(f"Mi número es: {numero}")
+
+#Exercise 9:
+
+print("hello world")
+
+"""
+-¿Cuáles son los 4 tipos de datos en Python?:
+cadenas, numeros enteros, numeros decimales, booleanos *hay mas tipos de datos como listas por ejemplo
+
+-¿Qué tipo de convención de nomenclatura debemos usar para las variables en Python?:
+en minusculas y si hay que separar palabras se hace con una barra baja, el nombre de esto no recuerdo. 
+
+-¿Qué es un heredoc de Python?:
+heredoc son datos multilinea que es lo que estoy usando yo ahora para responder, se pueden asignar a una variable y se convierten en string o usarse para comentarios.
+
+-¿Qué es la interpolación de cadenas?:
+Es el metodo que se usa para insertar datos dentro de una cadena (ejercicio 8)
+
+-¿Cuándo debemos usar comentarios en Python?:
+Cuando queramos comentar algo... 
+En este curso no recomiendan abusar de los comentarios pues pueden llevar a errores o quedarse obsoletos. Lo recomendable es usar variables descriptivas que hagan entender el código sin usar comentarios.
+
+-¿Cuáles son las diferencias entre las aplicaciones monolíticas y de microservicios?:
+Pues que las monoliticas se conforman dentro de una misma base y las de microservicios se dividen en diferentes bases.
+Monoliticas tienen sus pros, como que son mas sencillas de crear y mantener, y sus contras como que si falla algo en la aplicación esta puede crashear al completo.
+Las de microservicios tienen pros como que para trabajar en equipo cada persona puede hacerse cargo de una sección de la aplicación y si algo falla en la aplicación las demas partes de la aplicación pueden seguir funcionando. Como contra que su creación y mantenimiento son mas complicados.
+
+"""
+
+
+
+
